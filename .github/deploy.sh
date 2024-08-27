6bac9bfef008313c50896ffe64bfbfa7d3393eee
王东斌 <dongbin.wang@xtransfer.cn>
2024-08-27 17:35:17 +0800

    update

---
+++ .github/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo '开始执行部署任务'
+
+rm -rf ~/gh-projects/gateway
+
+git clone "https://github.com/onedb-document/gateway.git" ~/gh-projects/gateway
+
+echo '仓库准备完毕'
+
+cd ~/gh-projects/gateway
+
+docker-compose up -d
+
+echo '仓库准备完毕'
